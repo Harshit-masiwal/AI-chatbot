94301e2428753ecaff469353c82a997447220a27
Harshit Masiwal <harshitmasiwal@Harshits-MacBook-Air.local>
2026-04-03 11:25:01 +0530

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from langchain.chat_models import init_chat_model
 from langchain.tools import tool
 from langchain.agents import create_agent
+from langchain.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
@@ -13,9 +14,22 @@ app = FastAPI()
 
 class ChatRequest(BaseModel):
     message: str
+    user_id: str
 
 llm = init_chat_model("google_genai:gemini-2.5-flash-lite")
 
+system_msg = SystemMessage(
+    content="""
+You are an AI assistant.
+Remember user information from previous messages.
+If user tells their name, store it and use it later.
+Use weather tool when needed.
+"""
+)
+
+user_sessions = {}
+
+from langchain.tools import tool
 
 @tool
 def calculator(expression: str) -> str:
@@ -34,24 +48,41 @@ def geo_info(place: str) -> str:
     return f"{place} is a place on Earth 🌍"
 
 
-tools = [calculator, geo_info]
+@tool
+def weather(city: str) -> str:
+    """Get current weather of a city"""
+    return f"The weather in {city} is sunny ☀️"
+
+tools = [calculator, geo_info, weather]
 
 agent = create_agent(
     model=llm,
     tools=tools
 )
 
-
 @app.get("/")
 async def home():
     return {"message": "Server is running 🚀"}
 
-
 @app.post("/chat")
 async def chat(req: ChatRequest):
     try:
-        response = llm.invoke(req.message)
-        return {"response": response.content}
+        if req.user_id not in user_sessions:
+            user_sessions[req.user_id] = []
+
+        chat_history = user_sessions[req.user_id]
+
+        chat_history.append(HumanMessage(content=req.message))
+
+        response = agent.invoke({
+            "messages": [system_msg] + chat_history
+        })
+
+        ai_msg = response["messages"][-1]
+
+        chat_history.append(ai_msg)
+
+        return {"response": ai_msg.content}
+
     except Exception as e:
         return {"error": str(e)}
-    
